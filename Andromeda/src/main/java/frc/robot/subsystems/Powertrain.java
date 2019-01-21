28557f4c95737635aebeaaeeb07f0b0a3803088f
Jose-Rashest <42625558+Jose-Rashest@users.noreply.github.com>
2019-01-21 02:50:55 -0600

    Update Powertrain.java

--- Andromeda/src/main/java/frc/robot/subsystems/Powertrain.java
+++ Andromeda/src/main/java/frc/robot/subsystems/Powertrain.java
@@ -9,72 +9,65 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.DemandType;
+import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import frc.robot.RobotMap;
 
 /**
  * Add your docs here.
  */
 public class Powertrain extends Subsystem {
-  TalonSRX leftmotor1 = null,   rightmotor1 = null, 
-           leftmotor2 = null,   rightmotor2 = null;
+  WPI_TalonSRX leftMaster = null,   rightMaster = null, 
+           leftFollow = null,   rightFollow = null;
 
   ADXRS450_Gyro gyro;
 
-    // DifferentialDrive robot;
+    DifferentialDrive robotDrive;
 
   public Powertrain(){
-    leftmotor1 = new TalonSRX(RobotMap.powertrain_leftmotor1);
-    leftmotor2 = new TalonSRX(RobotMap.powertrain_leftmotor2);
-    rightmotor1 = new TalonSRX(RobotMap.powertrain_rightmotor1);
-    rightmotor2 = new TalonSRX(RobotMap.powertrain_rightmotor2);
+    leftMaster = new WPI_TalonSRX(RobotMap.powertrain_leftmotor1);
+    leftFollow = new WPI_TalonSRX(RobotMap.powertrain_leftmotor2);
+    rightMaster = new WPI_TalonSRX(RobotMap.powertrain_rightmotor1);
+    rightFollow = new WPI_TalonSRX(RobotMap.powertrain_rightmotor2);
 
-    leftmotor1.configFactoryDefault();
-    leftmotor2.configFactoryDefault();
-		rightmotor1.configFactoryDefault();
-    rightmotor2.configFactoryDefault();
+    robotDrive = new DifferentialDrive(leftMaster, rightMaster);
+
+    leftMaster.configFactoryDefault();
+    leftFollow.configFactoryDefault();
+		rightMaster.configFactoryDefault();
+    rightFollow.configFactoryDefault();
     
-    leftmotor1.setNeutralMode(NeutralMode.Brake);
-    leftmotor2.setNeutralMode(NeutralMode.Brake);
-    rightmotor1.setNeutralMode(NeutralMode.Brake);
-    rightmotor2.setNeutralMode(NeutralMode.Brake);
+    leftFollow.follow(leftMaster);
+    rightFollow.follow(rightMaster);
 
-    rightmotor1.setInverted(true);
-    rightmotor2.setInverted(true);
-
-    leftmotor2.set(ControlMode.Follower, RobotMap.powertrain_leftmotor1);
-    rightmotor2.set(ControlMode.Follower, RobotMap.powertrain_rightmotor1);
-
+    leftMaster.setInverted(false);
+    rightMaster.setInverted(true);
+    
+    leftFollow.setInverted(InvertType.FollowMaster);
+    rightFollow.setInverted(InvertType.FollowMaster);
   }
     public void arcadeDrive(double xSpeed, double ySpeed){
       /* Gamepad processing */
-      double forward = -1 * ySpeed;
-      double turn = xSpeed;	
-      forward = Deadband(forward);
-      turn = Deadband(turn);
+      double forw = -1 * ySpeed;
+      double turn = +1 * xSpeed;	
+      
+      if (Math.abs(forw) < 0.10) {
+        forw = 0;
+      }
+      if (Math.abs(turn) < 0.10) {
+        turn = 0;
+      }
 
-      /* Arcade Drive using PercentOutput along with Arbitrary Feed Forward supplied by turn */
-      leftmotor1.set(ControlMode.PercentOutput, forward, DemandType.ArbitraryFeedForward, +turn);
-      rightmotor1.set(ControlMode.PercentOutput, forward, DemandType.ArbitraryFeedForward, -turn);
+      robotDrive.arcadeDrive(forw, turn);
 
     }
 
-  double Deadband(double value) {
-    /* Upper deadband */
-    if (value >= +0.05) 
-      return value;
-    
-    /* Lower deadband */
-    if (value <= -0.05)
-      return value;
-    
-    /* Outside deadband */
-    return 0;
-  }
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
